b777008e7a2dd5b2301a295f1c837b4f2f48cd74
Martin Thiede <martin.thiede@esrlabs.com>
2013-11-20 18:46:15 +0100

    byte format parsing and checking

--- public/main.js
+++ public/main.js
@@ -1,5 +1,94 @@
 "use strict";
 
+function createFormChecker(service_input, did_rid_input, request_input) {
+
+  function parseBytes(string) {
+    var failed = false;
+    var byteStrs = [];
+    var bytes = [];
+
+    var parts = string.split(/[ \t]*[,;.][ \t]*|[ \t]+/);
+
+    $.each(parts, function(i, p) {
+      p = p.replace(/^0[xX]/, "");
+
+      if (p.length > 2 && (p.length % 2) > 0) {
+        failed = true;
+      } 
+      while (p.length > 2) {
+        byteStrs.push(p.slice(0, 2));
+        p = p.slice(2);
+      }
+      byteStrs.push(p);
+    });
+
+    $.each(byteStrs, function(i, b) {
+      if (b.match(/^[0-9a-fA-F]+$/)) {
+        bytes.push(parseInt(b, 16));
+      }
+      else {
+        failed = true;
+      }
+    })
+
+    if (failed) {
+      return false;
+    }
+    else {
+      return bytes;
+    }
+  }
+
+  function bytesToString(bytes) {
+    var str = "";
+    $.each(bytes, function(i, b) {
+      var s = b.toString(16);
+      if (s.length === 1) {
+        s = "0"+s;
+      }
+      if (i > 0) {
+        str += ",";
+      }
+      str += s;
+    });
+    return str;
+  }
+
+  function check_input(input) {
+    if (parseBytes(input.val()) === false) {
+      input.addClass("input_error");
+      return false;
+    }
+    else {
+      input.removeClass("input_error");
+      return true;
+    }
+  }
+
+  function update_request() {
+    var bytes = parseBytes(service_input.val()).concat(parseBytes(did_rid_input.val()));
+    request_input.val(bytesToString(bytes));
+  }
+
+  service_input.on("change, keyup", function(e) {
+    if (check_input(service_input) !== false) {
+      update_request();
+    }
+  });
+  did_rid_input.on("change, keyup", function(e) {
+    if (check_input(did_rid_input) !== false) {
+      update_request();
+    }
+  });
+  request_input.on("change, keyup", function(e) {
+    check_input(request_input);
+  });
+
+  check_input(service_input);
+  check_input(did_rid_input);
+  check_input(request_input);
+}
+
 function createConnector(channel_dropdown, connect_button, receive_handler) {
   var state = "disconnected";
 
